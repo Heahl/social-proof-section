96e6e8f64a1cff0a553a778643d5a70993837155
heahl <hendrik712.a@gmail.com>
2024-05-21 07:47:48 +0200

    final commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,15 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      colors: {
+        'very-dark-magenta': 'hsl(300, 43%, 22%)',
+        'soft-pink': 'hsl(333, 80%, 67%)',
+        'dark-grayish-magenta': 'hsl(303, 10%, 53%)',
+        'light-grayish-magenta': 'hsl(300, 24%, 96%)',
+        'white': 'hsl(0, 0%, 100%)'
+      },
       fontFamily: {
+        league: ["League Spartan", ...fontFamily.sans],
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
     },
